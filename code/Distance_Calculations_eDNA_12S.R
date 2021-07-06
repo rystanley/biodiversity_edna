bb8441cb8376cff379a47bc27c7e1eee72ef9193
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2021-07-05 23:23:49 -0300

    Code works for me

--- code/Distance_Calculations_eDNA_12S.R
+++ code/Distance_Calculations_eDNA_12S.R
@@ -18,7 +18,7 @@ library(rgeos)
   latlong <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
   CanProj <- "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=63.390675 +lon_0=-91.86666666666666 +x_0=6200000 +y_0=3000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 
-# I have to set the path  
+# I have to set the path  --- note you don't have to do this when using an R project 
   setwd("~/Documents/GitHub/biodiversity_edna")
   
 #Source functions for analysis ------------
